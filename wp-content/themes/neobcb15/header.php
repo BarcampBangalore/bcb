243029a1c89c37f9da0105b1740763a518154536
SathyaBhat <sathya@sathyasays.com>
2014-03-17 00:30:10 +0530

    Fixes #20, Update facebook opengraph image

--- wp-content/themes/neobcb15/header.php
+++ wp-content/themes/neobcb15/header.php
@@ -4,7 +4,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta property="og:title" content="Barcamp Bangalore Spring 2014" />
         <meta property="og:description" content="Barcamp Bangalore is an open event focused around people, ideas and collaboration. You don't want to miss this confluence of amazing minds. It's the largest unconference in India and there are talks on variety of topics like Technology, Design, Startups & Entrepreneurship, Business & Management, Photography, User Experience, Your life learnings, and a lot more... " />
-        <meta property="og:image" content="<?php bloginfo('template_url'); ?>/images/barcamp-logo_fbog.jpg" />
+        <meta property="og:image" content="<?php bloginfo('template_url'); ?>/images/barcamp-logo_fbog.png" />
         <title>Barcamp Bangalore Spring 2014<?php
 if (!is_home())
 {
