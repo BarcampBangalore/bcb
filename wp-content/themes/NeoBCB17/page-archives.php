2c0bef6e97f785a2b018656e04000d75ce6f5862
SathyaBhat <sathya@sathyasays.com>
2015-01-25 12:30:19 +0530

    New theme for new Barcamp

---
+++ wp-content/themes/NeoBCB17/page-archives.php
@@ -0,0 +1,462 @@
+<?php
+
+
+function insertSponsors($bcbname)
+{
+    switch ($bcbname)
+    {
+        case "bcb15":
+            ?>
+                            
+            <div class="archive_sponsors_wrapper">
+                <h2>Sponsors for <?php echo$bcbname  ?></h2>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Event Partner</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sap_logo.png" /></div>
+                </div>
+                
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Supported by</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/janastu_logo.gif" /></div>
+                </div>
+                
+                
+                
+                
+                
+            </div>
+
+            <?php
+            break;
+        
+        case "bcb14":
+            ?>
+                            
+            <div class="archive_sponsors_wrapper">
+                <h2>Sponsors for <?php echo$bcbname  ?></h2>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Event Partner</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sap_logo.png" /></div>
+                </div>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Silver Sponsor</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/intuit_blue.gif" /></div>
+                </div>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Community Partner</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/mozilla-firefox.png" /></div>
+                </div>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Community Partner</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/Ola_Cabs_Logo.png" /></div>
+                </div>
+                
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Supported by</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/janastu_logo.gif" /></div>
+                </div>
+                
+                
+                
+                
+                
+            </div>
+
+            <?php
+            break;
+        
+        case "bcb13":
+            ?>
+                            
+            <div class="archive_sponsors_wrapper">
+                <h2>Sponsors for <?php echo$bcbname  ?></h2>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Event Partner</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sap_logo.png" /></div>
+                </div>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Silver Sponsor</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/mowbly_logo.png" /></div>
+                </div>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Friend of Barcamp</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/coworkable_logo.jpg" /></div>
+                </div>
+            </div>
+
+            <?php
+            break;
+        
+        
+        
+        case "bcb12":
+            ?>
+                            
+            <div class="archive_sponsors_wrapper">
+                <h2>Sponsors for <?php echo$bcbname  ?></h2>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Event Partner</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sap_logo.png" /></div>
+                </div>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Gold Sponsor</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/microsoft_logo.png" /></div>
+                </div>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Friend of Barcamp</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/minjar_logo.png" /></div>
+                </div>
+            </div>
+
+            <?php
+            break;
+        
+        
+        case "bcb11":
+            ?>
+                            
+            <div class="archive_sponsors_wrapper">
+                <h2>Sponsors for <?php echo$bcbname  ?></h2>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Event Partner</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sap_logo.png" /></div>
+                </div>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Gold Sponsor</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/microsoft_logo.png" /></div>
+                </div>
+                
+            </div>
+
+            <?php
+            break;
+        
+        
+        case "bcb10":
+            ?>
+                            
+            <div class="archive_sponsors_wrapper">
+                <h2>Sponsors for <?php echo$bcbname  ?></h2>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Event Partner</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sap_logo.png" /></div>
+                </div>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Friend of Barcamp</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/fusioncharts_logo.png" /></div>
+                </div>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Friend of Barcamp</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/KRDS-Facecode.png" /></div>
+                </div>
+            </div>
+
+            <?php
+            break;
+        
+        
+        case "bcb9":
+            ?>
+                            
+            <div class="archive_sponsors_wrapper">
+                <h2>Sponsors for <?php echo$bcbname  ?></h2>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Event Partner</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/intuit_logo.gif" /></div>
+                </div>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Gold Sponsor</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/orangescape_logo.png" /></div>
+                </div>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Friend of Barcamp</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/fusioncharts_logo.png" /></div>
+                </div>
+            </div>
+
+            <?php
+            break;
+        
+        
+        case "bcb8":
+            ?>
+                            
+            <div class="archive_sponsors_wrapper">
+                <h2>Sponsors for <?php echo$bcbname  ?></h2>
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Event Partner</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/yahoo_logo_in.png" /></div>
+                </div>
+                
+            </div>
+
+            <?php
+            break;
+        
+        
+        
+        
+
+
+        default:
+            break;
+    }
+}
+
+get_header(); ?>
+
+<div id="sessionpage_wrapper" class="centered_background">
+
+    <script type="text/javascript">
+            
+            
+            
+        $(function(){
+                
+                
+            $.Isotope.prototype._getMasonryGutterColumns = function() {
+                var gutter = this.options.masonry.gutterWidth || 0;
+                containerWidth = this.element.parent().width();
+                this.masonry.columnWidth = this.options && this.options.masonry.columnWidth ||
+                    this.$filteredAtoms.outerWidth(true) ||
+                    containerWidth;
+                this.masonry.columnWidth += gutter;
+                this.masonry.cols = Math.floor(containerWidth / this.masonry.columnWidth);
+                this.masonry.cols = Math.max(this.masonry.cols, 1);
+            };
+ 
+            $.Isotope.prototype._masonryReset = function() {
+                this.masonry = {};
+                this._getMasonryGutterColumns();
+                var i = this.masonry.cols;
+                this.masonry.colYs = [];
+                while (i--) {
+                    this.masonry.colYs.push( 0 );
+                }
+            };
+ 
+            $.Isotope.prototype._masonryResizeChanged = function() {
+                var prevColCount = this.masonry.cols;
+                this._getMasonryGutterColumns();
+                return ( this.masonry.cols !== prevColCount );
+            };
+ 
+            $.Isotope.prototype._masonryGetContainerSize = function() {
+                var gutter = this.options.masonry.gutterWidth || 0;
+                var unusedCols = 0,
+                i = this.masonry.cols;
+                while ( --i ) {
+                    if ( this.masonry.colYs[i] !== 0 ) {
+                        break;
+                    }
+                    unusedCols++;
+                }
+                return {
+                    height : Math.max.apply( Math, this.masonry.colYs ),
+                    width : ((this.masonry.cols - unusedCols) * this.masonry.columnWidth) - gutter
+                };
+            };
+ 
+ 
+                
+                
+                
+            var $containers = $('.cards_parent_class');
+            // initialize
+            $containers.each(function(){
+                $(this).isotope({
+                    //columnWidth: 400,
+                    itemSelector: '.sessioncard',
+                    masonry : {
+                        columnWidth : 380,
+                        gutterWidth: 20
+                        
+                    }
+                    //gutterWidth: 10
+                });
+                
+                $(this).imagesLoaded(function(){
+                        
+                    $container.isotope('reLayout');
+                });
+                
+
+            });
+        
+            
+       
+            
+            
+            $("#archivelinks a").click(function(){
+                $("#archivelinks li").removeClass("archive_selected");
+                $(".archive_parent").hide();
+                var contentdiv = "#" + $(this).data("contentdiv");
+                $(contentdiv).show()
+                $(this).parent().addClass("archive_selected");
+                $(contentdiv + " .cards_parent_class").isotope('reLayout');
+                
+            });
+            
+            
+                    
+                    
+                    
+                    
+        });
+            
+    </script>
+
+
+
+    <div id="sessionpage_content">
+
+        <div id="sessionpage_header">
+            <div id="normalpage_title" class="yellowbg">
+                <h1 class="normalpage_heading"><?php the_title(); ?></h1>
+            </div>
+
+        </div>
+        <div style="clear: both"></div>
+
+        <div id="sessionpage_midsection">
+            <div id="generalpage_posttext">
+                <ul id="archivelinks">
+                    <li class="archive_selected"><a data-contentdiv="archive_bcb15">BCB15</a></li>
+                    <li><a  data-contentdiv="archive_bcb14">BCB14</a></li>
+                    <li><a  data-contentdiv="archive_bcb13">BCB13</a></li>
+                    <li><a  data-contentdiv="archive_bcb12">BCB12</a></li>
+                    <li><a  data-contentdiv="archive_bcb11">BCB11</a></li>
+                    <li><a  data-contentdiv="archive_bcb10">BCB10</a></li>
+                    <li><a  data-contentdiv="archive_bcb9">BCB9</a></li>
+                    <li><a  data-contentdiv="archive_bcb8">BCB8</a></li>
+                </ul>
+
+
+
+            </div>
+
+            <div style="clear: both"></div>
+        </div>
+        <?php
+        $techlash_categories = array(639, 787, 933, 1065);
+        $archive_categories = array(931 => 'bcb15', 785 => 'bcb14', 636 => 'bcb13', 479 => 'bcb12', 399 => 'bcb11', 324 => 'bcb10', 220 => 'bcb9', 3 => 'bcb8' );
+        foreach ($archive_categories as $archive_cat => $archive_catname):
+            ?>
+        <div id="archive_<?php echo $archive_catname; ?>" class="archive_parent" <?php if ($archive_catname != "bcb15")  echo 'style="display: none;"';  ?> >
+            <a id="archivepage_<?php echo $archive_catname;?>" ></a>
+            
+            
+            
+            
+            
+            <div id="techlash_cardswrapper">
+                
+                <?php insertSponsors($archive_catname); ?>
+                
+                <h2 id="techlash_sessionheading">Sessions for <?php echo get_cat_name( $archive_cat );  ?>  </h2>
+                <div id="cards_parent" class="cards_parent_class">
+                    <?php
+                    query_posts('cat=' . $archive_cat);
+
+                    while (have_posts()) : the_post();
+                    $is_techlash_session = false;
+                    $post_categories = wp_get_post_categories( get_the_ID() );
+
+                    foreach($post_categories as $c){
+                        if (array_search($c, $techlash_categories) !== FALSE)
+                        {
+                            $is_techlash_session = true;
+                        }
+                    }
+                    
+                        ?>
+
+                        <div class="sessioncard" id="sessioncard<?php the_ID(); ?>">
+                            
+                            
+                            
+
+                        <div class="sessioncard_user track_color_bg_archive">
+        <?php echo '<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_the_author_meta('user_nicename') . '</a>'; ?>
+
+                        </div>
+                        <div class="sessioncard_useravatar"><?php echo '<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_avatar(get_the_author_meta('ID'), 48) . '</a>'; ?><?php //echo '<img src="http://placeimg.com/48/48/any" />';     ?></div>
+                        <?php if (isset($_COOKIE['bcb_last_visit']) && (get_the_time('U') > ($_COOKIE['bcb_last_visit'] > strtotime("-2 days") ? strtotime("-2 days") : $_COOKIE['bcb_last_visit'] ))) : ?>
+                            <div class="sessioncard_newtag">new</div>     
+                            <?php endif; ?>
+                        <div class="sessioncard_head">
+
+
+                            <div class="sessioncard_title">
+                                <a href="<?php echo get_permalink(); ?>" class="track_color_archive">
+        <?php the_title(); ?>
+                                </a>
+                            </div>
+
+                            
+                            
+                            <div class="sessioncard_tagparent">
+                                <?php if ($is_techlash_session) : ?>
+                                    <div class="sessioncard_techlashtag">Techlash</div> 
+                                <?php endif; ?>
+                            </div>
+                        
+
+                            
+                        </div>
+                        <div class="sessioncard_footer">
+                            <div class="sessioncard_user_comments">
+
+                                <a class="sessioncard_attendees_link" href="<?php echo get_permalink(); ?>#attendees"><img class="sessioncard_meta_image" src="<?php bloginfo('template_url') ?>/images/users_icon.jpg" />
+                                    <span class="sessioncard_meta_text"><?php echo attending_users_count(get_the_ID()) ?></span></a>
+
+                                <a href="<?php echo get_permalink(); ?>#comments"><img class="sessioncard_meta_image" src="<?php bloginfo('template_url') ?>/images/comments_icon.jpg" />
+                                    <span class="sessioncard_meta_text"><?php comments_number('0', '1', '%'); ?></span></a>
+
+
+                            </div>
+
+                            
+                            <div style="clear: both"></div>
+                        </div>
+
+
+                    
+                            
+                            
+                            
+<!--                            <div class="sessioncard_head">
+
+                                <div class="sessioncard_title">
+                                    <a href="<?php echo get_permalink(); ?>">
+                                        <?php the_title(); ?>
+                                    </a>
+                                </div>
+                                <div class="sessioncard_user">
+                                    <?php echo '<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_the_author_meta('user_nicename') . '</a>'; ?> 
+                                </div>
+                                <div class="sessioncard_useravatar"><?php echo '<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_avatar( get_the_author_meta('ID'), 48 ) . '</a>'; ?><?php //echo '<img src="http://placeimg.com/48/48/any" />';        ?></div>
+                                <?php if ($is_techlash_session): ?>
+                                <img class="techlash_flag" src="<?php bloginfo('template_url')  ?>/images/techlash_flag.png" />
+                                                                
+                                <?php  endif; ?>
+                            </div>-->
+                        </div>
+
+
+                        <?php
+                    endwhile;
+                    ?>
+
+                </div>
+            </div>
+        </div>
+        <?php endforeach; ?>
+
+    </div>
+
+</div>
+
+
+<?php get_footer(); ?>
+        
