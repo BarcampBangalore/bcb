0d56a011525f9fae0dcabe94998a7256375472dc
SathyaBhat <sathya@sathyasays.com>
2015-08-31 21:28:35 +0530

    Theme prep for BCB Monsoon 2015
    last visit cookie time reduced to 2 days, 15 days was way too much

---
+++ wp-content/themes/NeoBCB18/sidebar.php
@@ -0,0 +1,82 @@
+<div id="card_sponsors_sidebar" class="bcb13_card">
+    <div class="bcb13_trans_bg">
+        <h2>Sponsors</h2>
+
+        <div class="bcb13_card_text">
+            Insert sponsor logos here
+        </div>
+    </div>
+</div>
+
+<div id="card_tags" class="bcb13_card">
+    <div class="bcb13_trans_bg">
+        <h2>#bcb13</h2>
+        <h3>one tag to rule them all</h3>
+
+        <div id="bcb13_card_text">
+            Help us spread the word. Use the folllowing tags<br><br>
+            <h4>Twitter</h4>
+            <i>#bcb13</i> <br><br>
+            
+            <h4>Blogging</h4>
+            <i>bcb13</i>, bcb, barcampbangalore, barcampbangalore13 <br><br>
+            
+            <h4>Flickr</h4>
+            <i>bcb13</i>, bcb, barcampbangalore, barcampbangalore13    
+        </div>
+    </div>
+</div>
+
+<?php if(false): ?>
+<ul>
+    <?php dynamic_sidebar('primary_widget_area'); ?>
+    <br>
+    
+    <li><h3>Buzz in Twitterverse</h3>
+        <div id="tweets_deck_wrapper">
+            <div id="tweets_deck"></div>
+        </div>
+    </li>
+    <li>
+        
+        <h3>Tell Friends</h3>
+        <a href="https://plus.google.com/share?url=http%3A%2F%2Fbarcampbangalore.org" onclick="javascript:window.open(this.href,
+            '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;"><img id="gplus_main" class="social_icon"  width="48" height="48" src="<?php bloginfo('template_url'); ?>/images/social/64/google_dark.png" /></a>
+        <a href="https://www.facebook.com/pages/Barcamp-Bangalore/172593159461976" target="_blank"><img id="fb_main" class="social_icon" width="48" height="48" src="<?php bloginfo('template_url'); ?>/images/social/64/facebook_dark.png" /></a>
+        <a href="https://twitter.com/share?text=I+am+attending+Barcamp+Bangalore+12+on+25th+August..+Where+will+you+be+%3F+%23bcb12&related=barcampbng%3ABarcamp+Bangalore+Official+Account" onclick="javascript:window.open(this.href,
+            '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=450,width=550');return false;"><img id="twitter_main" class="social_icon" width="48" height="48" src="<?php bloginfo('template_url'); ?>/images/social/64/twitter02_dark.png" /></a>
+    </li>
+    <li>
+        <h3>Our Sponsors</h3>
+        <div class="sponsor_div">
+            <a target="_blank" href="http://www.sap.com"><img style="padding-bottom: 10px;" width="90" src="<?php bloginfo('template_url'); ?>/images/sap_logo.png" /></a><br>
+            Event Partner
+        </div>
+        <div class="sponsor_div">
+            <a target="_blank" href="http://www.microsoft.com"><img style="padding-bottom: 30px;" width="90" src="<?php bloginfo('template_url'); ?>/images/ms-logo_white_small.png" /></a><br>
+            Gold Sponsor
+        </div>
+        <div class="sponsor_div">
+            <a target="_blank" href="http://www.minjar.com"><img style="padding-bottom: 0px;" width="70" src="<?php bloginfo('template_url'); ?>/images/Minjar_logo_small.png" /></a><br>
+            Friend of Barcamp
+        </div>
+        
+    </li>
+    <li>
+        <h3>#bcb12 - one tag to rule them all</h3>
+        Help us spread the word. Use the folllowing tags<br><br>
+        <b><u>Twitter</u></b><br>
+        <i>#bcb12</i> <br><br>
+        
+        <b><u>Blogging</u></b><br>
+        <i>bcb12</i>, bcb, barcampbangalore, barcampbangalore12 <br><br>
+        
+        <b><u>Flickr</u></b> <br>
+        <i>bcb12</i>, bcb, barcampbangalore, barcampbangalore12
+    </li>
+    
+    
+    
+    
+</ul>
+<?php endif; ?>
