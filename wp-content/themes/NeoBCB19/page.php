9dd2e8aefb8c6d0abde495db0cef23f1e2d6156b
Aman Manglik <amanmanglik@gmail.com>
2016-04-10 17:20:18 +0530

    Add-a-session page is restyled

--- wp-content/themes/NeoBCB19/page.php
+++ wp-content/themes/NeoBCB19/page.php
@@ -1,31 +1,27 @@
 <?php get_header(); ?>
            
-<div id="sessionpage_wrapper" class="centered_background">
+<div id="page_wrapper" class="centered_background">
     
    
     
     
     
-    <div id="sessionpage_content">
-
-        <div id="sessionpage_header">
-            <div id="sessionpage_title" class="yellowbg">
-                <h1><?php the_title(); ?></h1>
-            </div>
-            
-        </div>
-        <div style="clear: both"></div>
+    <div id="page_content">
         
-        <div id="sessionpage_midsection">
-            <div id="generalpage_posttext">
-                <?php the_post(); ?>
-                <?php the_content(); ?>
-                
-                
-                
+        <div class="container">
+            <div class="row">
+                <div id="page_title" class="single_page_box col-xs-12">
+                    <?php the_title(); ?>
+                </div>
             </div>
             
-            <div style="clear: both"></div>
+            <div class="row">
+                <div class="single_page_box col-xs-12">
+                    <?php the_post(); ?>
+                <?php the_content(); ?>
+                </div>
+            </div>
+            
         </div>
 
     </div>
