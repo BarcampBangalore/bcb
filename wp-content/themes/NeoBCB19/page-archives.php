631999d023800a254376bcd9c912a530ecdd5e59
Aman Manglik <amanmanglik@gmail.com>
2016-04-12 00:41:14 +0530

    Archives page styled

--- wp-content/themes/NeoBCB19/page-archives.php
+++ wp-content/themes/NeoBCB19/page-archives.php
@@ -5,6 +5,20 @@ function insertSponsors($bcbname)
 {
     switch ($bcbname)
     {
+        case "BCB Monsoon 2015":
+            ?>
+                            
+            <div class="archive_sponsors_wrapper">
+                <h2>Sponsors for <?php echo $bcbname; ?></h2>
+                
+                <div class="archive_sponsor">
+                    <h3 class="archive_sponsor_title">Supported by</h3>
+                    <div><img class="archive_sponsor_logo" src="<?php bloginfo('template_url')  ?>/images/sponz_logos/janastu_logo.gif" /></div>
+                </div>
+            </div>
+
+            <?php
+            break;
 
        case "bcbxah":
             ?>
@@ -269,96 +283,22 @@ get_header(); ?>
             
             
         $(function(){
-                
-                
-            $.Isotope.prototype._getMasonryGutterColumns = function() {
-                var gutter = this.options.masonry.gutterWidth || 0;
-                containerWidth = this.element.parent().width();
-                this.masonry.columnWidth = this.options && this.options.masonry.columnWidth ||
-                    this.$filteredAtoms.outerWidth(true) ||
-                    containerWidth;
-                this.masonry.columnWidth += gutter;
-                this.masonry.cols = Math.floor(containerWidth / this.masonry.columnWidth);
-                this.masonry.cols = Math.max(this.masonry.cols, 1);
-            };
- 
-            $.Isotope.prototype._masonryReset = function() {
-                this.masonry = {};
-                this._getMasonryGutterColumns();
-                var i = this.masonry.cols;
-                this.masonry.colYs = [];
-                while (i--) {
-                    this.masonry.colYs.push( 0 );
-                }
-            };
- 
-            $.Isotope.prototype._masonryResizeChanged = function() {
-                var prevColCount = this.masonry.cols;
-                this._getMasonryGutterColumns();
-                return ( this.masonry.cols !== prevColCount );
-            };
- 
-            $.Isotope.prototype._masonryGetContainerSize = function() {
-                var gutter = this.options.masonry.gutterWidth || 0;
-                var unusedCols = 0,
-                i = this.masonry.cols;
-                while ( --i ) {
-                    if ( this.masonry.colYs[i] !== 0 ) {
-                        break;
-                    }
-                    unusedCols++;
-                }
-                return {
-                    height : Math.max.apply( Math, this.masonry.colYs ),
-                    width : ((this.masonry.cols - unusedCols) * this.masonry.columnWidth) - gutter
-                };
-            };
- 
- 
-                
-                
-                
-            var $containers = $('.cards_parent_class');
-            // initialize
-            $containers.each(function(){
-                $(this).isotope({
-                    //columnWidth: 400,
-                    itemSelector: '.sessioncard',
-                    masonry : {
-                        columnWidth : 380,
-                        gutterWidth: 20
-                        
-                    }
-                    //gutterWidth: 10
-                });
-                
-                $(this).imagesLoaded(function(){
-                        
-                    $container.isotope('reLayout');
-                });
-                
-
-            });
-        
-            
-       
             
             
-            $("#archivelinks a").click(function(){
-                $("#archivelinks li").removeClass("archive_selected");
-                $(".archive_parent").hide();
+            
+                
+            $("#archives_page_bcb_selector a").click(function(){
+                $("#archives_page_bcb_selector li").removeClass("archive_selected");
+                $(".archives_parent").hide();
                 var contentdiv = "#" + $(this).data("contentdiv");
                 $(contentdiv).show()
                 $(this).parent().addClass("archive_selected");
-                $(contentdiv + " .cards_parent_class").isotope('reLayout');
                 
             });
             
+            $(".archives_current").click();
             
-                    
-                    
-                    
-                    
+                
         });
             
     </script>
@@ -371,179 +311,109 @@ get_header(); ?>
                     <div id="archives_page_title"><?php  the_title(); ?></div>
                     <div id="archives_page_bcb_selector_buttons">
                         <ul id="archives_page_bcb_selector">
-                        <li class="archive_selected"><a data-contentdiv="archive_bcbxah">BCBX AntHill</a></li>
-                        <li><a data-contentdiv="archive_bcb17">BCB17</a></li> 
-                        <li><a data-contentdiv="archive_bcb16">BCB16</a></li>
-                        <li><a data-contentdiv="archive_bcb15">BCB15</a></li>
-                        <li><a data-contentdiv="archive_bcb14">BCB14</a></li>
-                        <li><a data-contentdiv="archive_bcb13">BCB13</a></li>
-                        <li><a data-contentdiv="archive_bcb12">BCB12</a></li>
-                        <li><a data-contentdiv="archive_bcb11">BCB11</a></li>
-                        <li><a data-contentdiv="archive_bcb10">BCB10</a></li>
-                        <li><a data-contentdiv="archive_bcb9">BCB9</a></li>
-                        <li><a data-contentdiv="archive_bcb8">BCB8</a></li>
-                    </ul>
+                            <li class="archive_selected"><a class="archives_current" data-contentdiv="archive_1458">BCB Monsoon 2015</a></li> 
+                            <li><a data-contentdiv="archive_1366">BCBX AntHill</a></li>
+                            <li><a data-contentdiv="archive_1224">BCB17</a></li> 
+                            <li><a data-contentdiv="archive_1057">BCB16</a></li>
+                            <li><a data-contentdiv="archive_931">BCB15</a></li>
+                            <li><a data-contentdiv="archive_785">BCB14</a></li>
+                            <li><a data-contentdiv="archive_636">BCB13</a></li>
+                            <li><a data-contentdiv="archive_479">BCB12</a></li>
+                            <li><a data-contentdiv="archive_399">BCB11</a></li>
+                            <li><a data-contentdiv="archive_324">BCB10</a></li>
+                            <li><a data-contentdiv="archive_220">BCB9</a></li>
+                            <li><a data-contentdiv="archive_3">BCB8</a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
             
             
-            
-        </div>
-        
-    </div>
-    
-    
-    
-
-    <div id="sessionpage_content">
-
-        <div id="sessionpage_header">
-            <div id="normalpage_title" class="yellowbg">
-                <h1 class="normalpage_heading"><?php the_title(); ?></h1>
-            </div>
-
-        </div>
-        <div style="clear: both"></div>
-
-        <div id="sessionpage_midsection">
-            <div id="generalpage_posttext">
-                <ul id="archivelinks">
-                    <li class="archive_selected"><a data-contentdiv="archive_bcbxah">BCBX AntHill</a></li>
-                    <li><a data-contentdiv="archive_bcb17">BCB17</a></li> 
-                    <li><a data-contentdiv="archive_bcb16">BCB16</a></li>
-                    <li><a data-contentdiv="archive_bcb15">BCB15</a></li>
-                    <li><a data-contentdiv="archive_bcb14">BCB14</a></li>
-                    <li><a data-contentdiv="archive_bcb13">BCB13</a></li>
-                    <li><a data-contentdiv="archive_bcb12">BCB12</a></li>
-                    <li><a data-contentdiv="archive_bcb11">BCB11</a></li>
-                    <li><a data-contentdiv="archive_bcb10">BCB10</a></li>
-                    <li><a data-contentdiv="archive_bcb9">BCB9</a></li>
-                    <li><a data-contentdiv="archive_bcb8">BCB8</a></li>
-                </ul>
-
-
-
-            </div>
-
-            <div style="clear: both"></div>
-        </div>
-        <?php
-        $techlash_categories = array(639, 787, 933, 1065, 1232, 1465);
-        $archive_categories = array(1556 => "BCB Spring 2016", 1458 => "BCB Monsoon 2015", 1366 => 'bcbxah', 1224 => 'bcb17', 1057 => 'bcb16', 931 => 'bcb15', 785 => 'bcb14', 636 => 'bcb13', 479 => 'bcb12', 399 => 'bcb11', 324 => 'bcb10', 220 => 'bcb9', 3 => 'bcb8' );
-        foreach ($archive_categories as $archive_cat => $archive_catname):
+            <?php
+            $techlash_categories = array(639, 787, 933, 1065, 1232, 1465);
+            $archive_categories = array(1458 => "BCB Monsoon 2015", 1366 => 'bcbxah', 
+                1224 => 'bcb17', 1057 => 'bcb16', 931 => 'bcb15', 
+                785 => 'bcb14', 636 => 'bcb13', 479 => 'bcb12',
+                399 => 'bcb11', 324 => 'bcb10', 220 => 'bcb9', 3 => 'bcb8' );
+            foreach ($archive_categories as $archive_cat => $archive_catname):
             ?>
-        <div id="archive_<?php echo $archive_catname; ?>" class="archive_parent" <?php if ($archive_catname != "bcbxah")  echo 'style="display: none;"';  ?> >
-            <a id="archivepage_<?php echo $archive_catname;?>" ></a>
-            
-            
-            
-            
-            
-            <div id="techlash_cardswrapper">
-                
-                <?php insertSponsors($archive_catname); ?>
-                
-                <h2 id="techlash_sessionheading">Sessions for <?php echo get_cat_name( $archive_cat );  ?>  </h2>
-                <div id="cards_parent" class="cards_parent_class">
-                    <?php
-                    query_posts('cat=' . $archive_cat);
+            <div id="archive_<?php echo $archive_cat; ?>" class="archives_parent" >
+                <div class="row">
+                    <div class="single_page_box">
+                        <?php insertSponsors($archive_catname); ?>
+                    </div>
+                </div>
 
-                    while (have_posts()) : the_post();
-                    $is_techlash_session = false;
-                    $post_categories = wp_get_post_categories( get_the_ID() );
-
-                    foreach($post_categories as $c){
-                        if (array_search($c, $techlash_categories) !== FALSE)
-                        {
-                            $is_techlash_session = true;
-                        }
-                    }
-                    
-                        ?>
-
-                        <div class="sessioncard" id="sessioncard<?php the_ID(); ?>">
-                            
-                            
-                            
-
-                        <div class="sessioncard_user track_color_bg_archive">
-        <?php echo '<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_the_author_meta('user_nicename') . '</a>'; ?>
-
-                        </div>
-                        <div class="sessioncard_useravatar"><?php echo '<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_avatar(get_the_author_meta('ID'), 48) . '</a>'; ?><?php //echo '<img src="http://placeimg.com/48/48/any" />';     ?></div>
-                        <?php if (isset($_COOKIE['bcb_last_visit']) && (get_the_time('U') > ($_COOKIE['bcb_last_visit'] > strtotime("-2 days") ? strtotime("-2 days") : $_COOKIE['bcb_last_visit'] ))) : ?>
-                            <div class="sessioncard_newtag">new</div>     
-                            <?php endif; ?>
-                        <div class="sessioncard_head">
-
-
-                            <div class="sessioncard_title">
-                                <a href="<?php echo get_permalink(); ?>" class="track_color_archive">
-        <?php the_title(); ?>
-                                </a>
-                            </div>
-
-                            
-                            
-                            <div class="sessioncard_tagparent">
-                                <?php if ($is_techlash_session) : ?>
-                                    <div class="sessioncard_techlashtag">Techlash</div> 
-                                <?php endif; ?>
-                            </div>
-                        
-
-                            
-                        </div>
-                        <div class="sessioncard_footer">
-                            <div class="sessioncard_user_comments">
-
-                                <a class="sessioncard_attendees_link" href="<?php echo get_permalink(); ?>#attendees"><img class="sessioncard_meta_image" src="<?php bloginfo('template_url') ?>/images/users_icon.jpg" />
-                                    <span class="sessioncard_meta_text"><?php echo attending_users_count(get_the_ID()) ?></span></a>
-
-                                <a href="<?php echo get_permalink(); ?>#comments"><img class="sessioncard_meta_image" src="<?php bloginfo('template_url') ?>/images/comments_icon.jpg" />
-                                    <span class="sessioncard_meta_text"><?php comments_number('0', '1', '%'); ?></span></a>
-
-
-                            </div>
-
-                            
-                            <div style="clear: both"></div>
-                        </div>
-
-
-                    
-                            
-                            
-                            
-<!--                            <div class="sessioncard_head">
-
-                                <div class="sessioncard_title">
-                                    <a href="<?php echo get_permalink(); ?>">
-                                        <?php the_title(); ?>
-                                    </a>
-                                </div>
-                                <div class="sessioncard_user">
-                                    <?php echo '<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_the_author_meta('user_nicename') . '</a>'; ?> 
-                                </div>
-                                <div class="sessioncard_useravatar"><?php echo '<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_avatar( get_the_author_meta('ID'), 48 ) . '</a>'; ?><?php //echo '<img src="http://placeimg.com/48/48/any" />';        ?></div>
-                                <?php if ($is_techlash_session): ?>
-                                <img class="techlash_flag" src="<?php bloginfo('template_url')  ?>/images/techlash_flag.png" />
-                                                                
-                                <?php  endif; ?>
-                            </div>-->
-                        </div>
-
+                <div class="row">
 
                         <?php
-                    endwhile;
-                    ?>
+                        query_posts('cat=' . $archive_cat);
+                        while (have_posts()) : the_post(); ?>
+
+
+                            <div class="sessions_page_card col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                                <div class="sessions_page_card_content container-fluid track_color_border_archive">
+                                    <div class="row">
+                                        <div class="sessions_page_card_avatar col-xs-2">
+                                            <?php echo '<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_avatar(get_the_author_meta('ID'), 48) . '</a>'; ?><?php //echo '<img src="http://placeimg.com/48/48/any" />';     ?>
+                                        </div>
+                                        <div class="sessions_page_card_right_column track_color_border_archive col-xs-10">
+                                            <div class="sessions_page_card_title">
+
+                                                <?php
+                                                    $titlestr = get_the_title();
+                                                    $title_trimmed = false;
+                                                    if (strlen($titlestr) > 50) {
+                                                        $titlestr = substr($titlestr, 0, 50)."...";
+                                                        $title_trimmed = true;
+                                                    }
+                                                ?>
+                                                <a href="<?php echo get_permalink(); ?>" <?php if ($title_trimmed) {echo 'title="'.get_the_title().'"';} ?>>
+                                                    <?php echo $titlestr; ?>
+                                                </a>
+                                            </div>
+                                            <div class="sessions_page_card_author">
+                                                <?php echo '<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_the_author_meta('user_nicename') . '</a>'; ?>
+                                            </div>
+                                            <div class="sessions_page_card_bottomarea">
+
+                                                <div class="sessions_page_card_comments_stats">
+                                                    <div class="sessions_page_card_comments_icon">
+                                                        <img src="<?php echo get_bloginfo('template_url').'/images/icons-21.png' ?>" />
+                                                    </div>
+                                                    <div class="sessions_page_card_comments_count">
+                                                        <?php comments_number('0', '1', '%'); ?>
+                                                    </div>
+                                                </div>
+
+                                                <div class="sessions_page_card_attendees_stats">
+                                                    <div class="sessions_page_card_attendees_icon">
+                                                        <img src="<?php echo get_bloginfo('template_url').'/images/icons-20.png' ?>" />
+                                                    </div>
+                                                    <div id="sessions_page_card_attendees_count_<?php echo get_the_ID();  ?>" class="sessions_page_card_attendees_count">
+                                                        <?php echo attending_users_count(get_the_ID()) ?>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+
+
+                        <?php endwhile; ?>
 
                 </div>
             </div>
+            
+            <?php endforeach; ?>
+            
         </div>
-        <?php endforeach; ?>
-
+        
     </div>
 
 </div>
